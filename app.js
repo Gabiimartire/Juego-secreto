c6c6fcfef9818cf502af8ea8165e429cacf9f769
gabiimartire <gabbiimartire3@gmail.com>
2025-09-13 02:17:40 -0300

    ahora cuando se gana se desactiva el intentar y tambien el escuchador del enter hasta que se aprete reiniciar juego

--- app.js
+++ app.js
@@ -11,10 +11,12 @@ function asignarTextoElemento(elemento, texto) {
 }
 function verificarIntento() {
     let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-    
+    let input = document.getElementById('valorUsuario')
     if (numeroDeUsuario === numeroSecreto) {
         asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById('verificar').setAttribute('disabled', true);
+        pausarEvento(input)
     } else {
         //El usuario no acertó.
         if (numeroDeUsuario > numeroSecreto) {
@@ -27,19 +29,30 @@ function verificarIntento() {
     }
     return;
 }
-    //creo un addeventlistener al input para que me verifique el intento si apreto el enter tambien
-let numeroDeUsuario = document.getElementById('valorUsuario').addEventListener('keypress', function (e) {
+//creo un addeventlistener al input para que me verifique el intento si apreto el enter tambien
+let input = document.getElementById('valorUsuario');
+input.addEventListener('keypress', enter);
+
+function enter(e){
     //e -> es el evento. Y si la tecla del evento keypress es enter, verifica el intento
     if (e.key === 'Enter') {
       verificarIntento()
-    }})
+    }}
+    
+function pausarEvento(input) {
+  input.removeEventListener('keypress', enter);
+}
+
+function reanudarEvento(input) {
+  input.addEventListener('keypress', enter);
+}
+
 function limpiarCaja() {
     document.querySelector('#valorUsuario').value = '';
 }
 
 function generarNumeroSecreto() {
     let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
-
     console.log(numeroGenerado);
     console.log(listaNumerosSorteados);
     //Si ya sorteamos todos los números
@@ -55,7 +68,6 @@ function generarNumeroSecreto() {
         }
     }
 }
-
 function condicionesIniciales() {
     asignarTextoElemento('h1','Juego del número secreto!');
     asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
@@ -72,5 +84,9 @@ function reiniciarJuego() {
     condicionesIniciales();
     //Deshabilitar el botón de nuevo juego
     document.querySelector('#reiniciar').setAttribute('disabled','true');   
+    //Habilitar el boton intentar otra vez
+    document.getElementById('verificar').removeAttribute('disabled');
+    //el escuchador del enter en el input vuelve
+    reanudarEvento(input)
 }
 condicionesIniciales();
